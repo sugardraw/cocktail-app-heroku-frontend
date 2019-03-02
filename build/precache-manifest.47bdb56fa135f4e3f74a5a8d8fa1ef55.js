21a295a8bdaa0a4f548a7458f8c5d9a3e59f8e4c
sergio <sergiousl@yahoo.com>
2019-03-02 11:30:47 +0100

    succesfully deployed in heroku

---
+++ build/precache-manifest.47bdb56fa135f4e3f74a5a8d8fa1ef55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e0afd90872c4165f444",
+    "url": "/static/js/main.7e0afd90.chunk.js"
+  },
+  {
+    "revision": "230efd2f906fbbedc422",
+    "url": "/static/js/1.230efd2f.chunk.js"
+  },
+  {
+    "revision": "7e0afd90872c4165f444",
+    "url": "/static/css/main.5ecf78df.chunk.css"
+  },
+  {
+    "revision": "690559499c8ee2de672d3353cce8c6c6",
+    "url": "/index.html"
+  }
+];
